3c69ba688787b8f79b5c24a522684af5b4f734f6
Rokszin Roland <rokszinroland@gmail.com>
2025-01-29 10:45:37 +0100

    Project Base Skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaymentComponent } from './payment/payment.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'payments', pathMatch: 'full' },
+  { path: 'payments', component: PaymentComponent },
+  { path: '**', redirectTo: 'payments', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
